825dabbf727ea3461d2236b93def34525babd3ac
rambabu199 <93924576+rambabu199@users.noreply.github.com>
2022-04-08 21:41:07 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,16 @@
+public class OddEven {
+
+	public static void main(String[] args) {
+		
+		int num = 72;
+		
+		if (num % 2 == 0) {
+			System.out.print("Even");
+		}else {
+			System.out.print("Odd");
+		}
+		
+
+	}
+
+}
