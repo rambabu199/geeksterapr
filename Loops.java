d58c3503983eff8f52a92f090d33850afa6661fa
rambabu199 <93924576+rambabu199@users.noreply.github.com>
2022-04-08 21:34:53 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+
+	public static void main(String[] args) {
+		int num=1;
+		for (int i=0;i<4;i++) {
+			System.out.println(num);
+			num=num+1; 
+		}
+		
+		System.out.println(num); 
+		
+		
+	}
+}
